596cc11f84a4aa8c4db59efcbb54c0e93743d267
selimRU <selim.soha92@gmail.com>
2023-10-19 01:06:22 +0600

    add cart completed using database

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
 
 
@@ -30,20 +30,33 @@ async function run() {
     try {
         await client.connect();
         const productCollections = client.db("brandShop").collection("products");
+        const cartCollections = client.db("brandShop").collection("cart");
 
         app.get('/products', async (req, res) => {
             const query = {};
             const result = await productCollections.find(query).toArray()
             res.send(result)
         })
-        app.get('/products/:name', async (req, res) => {
-            const query = { name: req.params.name }
-            const result = await productCollections.find(query).toArray()
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await productCollections.findOne(query)
             res.send(result)
         })
+        app.get('/cart', async (req, res) => {
+            const query = {};
+            const result = await cartCollections.find(query).toArray()
+            res.send(result)
+        })
+
         app.post('/products', async (req, res) => {
             const product = req.body
             const result = await productCollections.insertOne(product)
+            res.send(result)
+        })
+        app.post('/cart', async (req, res) => {
+            const product = req.body
+            const result = await cartCollections.insertOne(product)
             res.send(result)
         })
 
